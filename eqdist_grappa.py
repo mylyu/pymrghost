9cd141e515079ad96b7ffb0347d11bbfa2b5bd65
Mengye LYU <lvmengye@gmail.com>
2024-04-15 20:16:25 +0800

    Update eqdist_grappa.py

--- eqdist_grappa.py
+++ eqdist_grappa.py
@@ -294,4 +294,4 @@ def GRAPPA_interpolate_kSpace_2d(undersampled_kspace_kxkyc, acc_factors_2d, bloc
 
     image_recon_sos = np.sqrt(np.sum(np.abs(np.fft.ifft2(kspace_coils)) ** 2, axis=2))
 
-    return kspace_coils， image_recon_sos
+    return kspace_coils, image_recon_sos
